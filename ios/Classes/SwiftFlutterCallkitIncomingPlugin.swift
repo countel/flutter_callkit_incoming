3f85feac0a6023e0f6e17a0145f466b36a980bb0
cosnomi <cosnomi@cosnomi.com>
2023-03-07 19:26:45 +0900

    Don't sleep before fulfilling

--- ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
+++ ios/Classes/SwiftFlutterCallkitIncomingPlugin.swift
@@ -387,9 +387,7 @@ public class SwiftFlutterCallkitIncomingPlugin: NSObject, FlutterPlugin, CXProvi
     private func checkUnlockedAndFulfill(action: CXAnswerCallAction, counter: Int) {
         if UIApplication.shared.isProtectedDataAvailable {
             sendEvent(SwiftFlutterCallkitIncomingPlugin.ACTION_CALL_ACCEPT, self.data?.toJSON())
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(5000)) {
-                action.fulfill()
-            }
+            action.fulfill()
     } else if counter > 180 { // fail if waiting for more then 3 minutes
         action.fail()
     } else {
